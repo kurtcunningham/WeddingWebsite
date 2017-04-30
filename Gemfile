370ab51fdd2fe9821f092e08fea6d23b3472a5fa
kurtcunningham <cunningham.kurt@gmail.com>
2017-04-30 10:45:41 -0500

    adding colors and type

--- Gemfile
+++ Gemfile
@@ -1,6 +1,16 @@
 source 'https://rubygems.org'
 
+# Middleman Gems
 gem 'middleman', '~> 4.2'
 gem 'middleman-autoprefixer', '~> 2.7'
+gem 'middleman-minify-html'
+# gem 'middleman-deploy', '~> 1.0'
+
+# Config Gems
 gem 'tzinfo-data', platforms: [:mswin, :mingw, :jruby]
 gem 'wdm', '~> 0.1', platforms: [:mswin, :mingw]
+
+# Added Gems
+gem 'bourbon'
+gem 'neat'
+gem 'nokogiri'
--- config.rb
+++ config.rb
@@ -5,42 +5,28 @@ activate :autoprefixer do |prefix|
   prefix.browsers = "last 2 versions"
 end
 
-# Layouts
-# https://middlemanapp.com/basics/layouts/
+# Markdown
+set :markdown_engine, :kramdown
 
-# Per-page layout changes
-page '/*.xml', layout: false
-page '/*.json', layout: false
-page '/*.txt', layout: false
-
-# With alternative layout
-# page '/path/to/file.html', layout: 'other_layout'
-
-# Proxy pages
-# https://middlemanapp.com/advanced/dynamic-pages/
-
-# proxy(
-#   '/this-page-has-no-template.html',
-#   '/template-file.html',
-#   locals: {
-#     which_fake_page: 'Rendering a fake page with a local variable'
-#   },
-# )
-
-# Helpers
-# Methods defined in the helpers block are available in templates
-# https://middlemanapp.com/basics/helper-methods/
-
-# helpers do
-#   def some_helper
-#     'Helping'
-#   end
-# end
+# Directory Settings
+set :css_dir,    'stylesheets'
+set :js_dir,     'scripts'
+set :images_dir, 'images'
+set :relative_links, false
 
 # Build-specific configuration
-# https://middlemanapp.com/advanced/configuration/#environment-specific-settings
+configure :build do
+  activate :minify_css
+  activate :minify_javascript
+  activate :minify_html
+  activate :directory_indexes
+  activate :gzip
+  activate :imageoptim, pngout: false
+  activate :relative_assets
+end
 
-# configure :build do
-#   activate :minify_css
-#   activate :minify_javascript
+# Deploy To GH-Pages
+# activate :deploy do |deploy|
+#   deploy.deploy_method = :git
+#   deploy.build_before = true
 # end
